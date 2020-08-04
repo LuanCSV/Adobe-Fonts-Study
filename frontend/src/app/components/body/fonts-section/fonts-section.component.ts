2d95bf6bb67ebdbf48df0ac6b1ccc3a0ebcd17d2
Luan Vasconcelos <luancsvasconcelos@hotmail.com>
2020-08-04 10:43:49 -0300

    Ajustes na seção de fonts

--- frontend/src/app/components/body/fonts-section/fonts-section.component.ts
+++ frontend/src/app/components/body/fonts-section/fonts-section.component.ts
@@ -8,19 +8,53 @@ import { Component, OnInit } from '@angular/core';
 export class FontsSectionComponent implements OnInit {
 
   iconFavorite = 'favorite_border';
-  isFavorite = false;
+  cards = [
+    {
+      fontImg: 'https://increasify.com.au/wp-content/uploads/2016/08/default-image.png',
+      fontTitle: 'Acumin',
+      fontSubtitle: '9 faves from designer Tad Carpenter',
+      fontInfo: '44 fonts • 86 fonts with CC',
+      isFavorite: this.iconFavorite,
+      isFav: false
+    },
+    {
+      fontImg: 'https://increasify.com.au/wp-content/uploads/2016/08/default-image.png',
+      fontTitle: 'Cortado',
+      fontSubtitle: 'XYZ Type',
+      fontInfo: '1 font with CC',
+      isFavorite: this.iconFavorite,
+      isFav: false
+    },
+    {
+      fontImg: 'https://increasify.com.au/wp-content/uploads/2016/08/default-image.png',
+      fontTitle: 'Sloop Script',
+      fontSubtitle: 'Lipton Letter Design',
+      fontInfo: '12 fonts with CC',
+      isFavorite: this.iconFavorite,
+      isFav: false
+    },
+    {
+      fontImg: 'https://increasify.com.au/wp-content/uploads/2016/08/default-image.png',
+      fontTitle: 'Masqualero',
+      fontSubtitle: 'Monotype',
+      fontInfo: '14 fonts with CC',
+      isFavorite: this.iconFavorite,
+      isFav: false
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  toggleFavorite() {
-    if (!this.isFavorite) {
-      this.iconFavorite = 'favorite';
-      this.isFavorite = true;
-    } else if (this.isFavorite) {
-      this.isFavorite = false;
-      this.iconFavorite = 'favorite_border';
-    }
+  toggleFavorite(card): void {
+      if (card.isFav === false) {
+        card.isFavorite = 'favorite';
+        card.isFav = true;
+      } else {
+        card.isFavorite = 'favorite_border';
+        card.isFav = false;
+      }
   }
 }
