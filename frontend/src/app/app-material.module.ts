f0d2640a5724399b9b28c858fbb4b61e9b036e65
LuanCSV <luancsvasconcelos@gotmail.com>
2023-07-09 10:49:25 -0300

    fix: dependecies update

--- frontend/src/app/app-material.module.ts
+++ frontend/src/app/app-material.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import {
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatListModule,
-    MatTooltipModule,
-    MatCardModule
-  } from '@angular/material';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
     exports: [
